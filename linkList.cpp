46a7613b21ac9c3c2c094d1c848bc73d4439a3df
‘Maverick <1097249217@qq.com>
2019-11-06 11:10:46 +0800

    over

---
+++ linkList.cpp
@@ -0,0 +1,126 @@
+#include "linkList.h"
+
+template<class Type>
+linklist<Type>::linklist(){
+    init();
+}
+
+template<class Type>
+bool linklist<Type>::Isempty(){
+    if(count==0)
+        return true;
+    else return false;
+}
+
+template<class Type>
+int linklist<Type>::Length(){
+    return count;
+}
+
+template<class Type>
+void linklist<Type>::Clear(){
+    while(head->next!=head){
+        Type a1;
+        DeleteElem(count,a1);//如果头结点next指针不为NULL，就继续删除
+    }
+}
+
+template<class Type>
+void linklist<Type>::Traverse(void(*visit)(const Type&)){
+    Node<Type>* cur=head->next;
+    while(cur!=NULL&&cur!=head){
+        (*visit)(cur->data);//对每个元素调用*visit
+        cur=cur->next;
+    }
+}
+
+template<class Type>
+Type linklist<Type>::GetPostionElem(int position){
+    Node<Type>* cur=head->next;
+    if(cur!=NULL){
+        for(int i=0;i<position;i++){
+            cur=cur->next;
+        }
+        return cur->data;
+    }
+    else return NULL;
+}
+
+//插入到当前序号的下一个，如果是0就在头节点后面直接插入
+template<class Type>
+int linklist<Type>::InsertElem(const Type &Elem,int position){
+    if(position<0||position>Length()+1){
+        return 0;
+    }
+    else{
+        Node<Type>*tmpPtr,*nextPtr,*newPtr;
+        tmpPtr=GetPositionPtr(position-1);
+        nextPtr=tmpPtr->next;
+        newPtr=new Node<Type>(Elem,tmpPtr,nextPtr);//生成新节点
+        tmpPtr->next=newPtr;
+        nextPtr->back=newPtr;
+        curposition=position;
+        curPtr=newPtr;
+        count++;
+        return 1;
+    }
+}
+
+template<class Type>
+int linklist<Type>::DeleteElem(int position,Type &Elem){
+    if(!Isempty()){
+        if(position>0&&position<=Length()){
+            Node<Type>* tmpPtr=GetPositionPtr(position-1);//去取出指向position的指针
+            tmpPtr->back->next=tmpPtr->next;//前一个指针的后继修改
+            tmpPtr->next->back=tmpPtr->back;//后一个指针的前驱修改
+            Elem=tmpPtr->data;//用Elem来返回删除的元素
+            if(position==Length()){
+                //删除尾节点,当前节点变为头节点
+                curposition=0;
+                curPtr=head;
+            }
+            else{
+                curposition=position;//设置当前位序号
+                curPtr=tmpPtr->next;//设置当前位置指向的指针
+            }
+            count--;
+            delete tmpPtr;
+        }
+        return 1;
+    }
+    else return 0;
+}
+/*
+template<class Type>
+int linklist<Type>::DeleteElem(){
+    if(!Isempty()){
+        Node<Type>* cur=head;
+        for(int i=0;i<Length();i++){
+            cur=cur->next;
+        }
+        delete cur;
+        return 1;
+    }
+    return 0;
+}
+*/
+
+template<class Type>
+int linklist<Type>::SetElem(int position, const Type &Elem){
+    if(Isempty()) return 0;
+    Node<Type>* cur=GetPositionPtr(position);
+    cur->data=Elem;
+    return 1;
+}
+
+template<class Type>
+linklist<Type>::~linklist(){
+    Clear();
+    delete head;
+}
+
+template<class Type>
+linklist<Type>::linklist(linklist<Type>&copy){
+    head=copy.head;
+}
+
---
+++ linkList.h
@@ -0,0 +1,75 @@
+#ifndef _LINKLIST_H_
+
+#define _LINKLIST_H_
+#include <stddef.h>
+
+template<class Type>
+struct Node
+{
+    Type data; //数据域
+    Node<Type> *back;//前驱指针域
+    Node<Type> *next; //后继指针域
+
+    Node(){
+        next=NULL;
+    } //构造函数模板
+    Node(Type item,Node<Type>*linkback = NULL,Node<Type>*linknext=NULL){
+        next=linknext;
+        back=linkback;
+        data=item;
+    } //已知一个数据，或者指针也知道，新建一个结点
+};
+
+template<class Type>
+class linklist{
+    protected:
+        Node<Type>* head;
+        mutable int curposition;//当前位置
+        mutable Node<Type>* curPtr;//指向当前位置指针
+        int count;//元素个数
+        void init(){
+            head = new Node<Type>;//构造头指针
+            head->next=head;//头节点初始化为循环链表
+            curPtr=head; curposition=0;//初始化当前指针指向头节点
+            count=0;//初始化元素个数
+        }
+        //辅助函数，返回指向第position个结点的指针
+        Node<Type>* GetPositionPtr(int position){
+            if(curposition<position){
+                //当前位置在查找位置之前，向后查找
+                for(;curposition<position;curposition++){
+                    curPtr=curPtr->next;
+                }
+            }
+            else if(curposition>position){
+                //当前位置在查找位置之后
+                for(;curposition>position;curposition--){
+                    curPtr->back;
+                }
+            }
+            //position=curposition
+            return curPtr;
+        }
+    public:
+        linklist();//无参构造函数
+        virtual ~linklist();//析构函数
+        bool Isempty();//判断是否为空
+        int Length();//返回线性表长度
+        void Clear();//清空线性表
+        void Traverse(void(*visit)(const Type &));//遍历线性表
+        Type GetPostionElem(int position);//获得指定位置的元素值
+        int InsertElem(const Type &Elem,int position);//插入指定元素到指定为位置
+        int DeleteElem(int position,Type &Elem);//删除指定位置元素
+        int DeleteElem();//直接删除队尾元素
+        int SetElem(int position,const Type &Elem);//设置指定位置元素
+        linklist(linklist<Type>&copy);//复制构造函数模板
+        linklist<Type> &operator =(const linklist<Type>&copy)//重载=
+        {
+            head=copy.head;
+        }        
+};
+
+
+
+
+#endif
